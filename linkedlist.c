f77f8f006ac815dd543eac8d78372d2a00351c69
Niharikaaa-1 <2024a6r004@mietjammu.in>
2025-04-04 01:18:50 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -65,6 +65,95 @@ void InsertNodeAtAnyPosition(int ele, int pos) {
     }
 }
 
+void DeleteNodeFromBeg() {
+    if (head == NULL) {
+        printf("List is empty, nothing to delete!\n");
+        return;
+    }
+    Node* temp = head; // Store the current head
+    head = head->next; // Move head to the next node
+    free(temp); // Free the old head
+    printf("Node deleted from the beginning.\n");    
+}
+
+void DeleteNodeFromEnd(){
+    if (head == NULL) {
+        printf("List is empty, nothing to delete!\n");
+        return;
+    }
+    if (head->next == NULL) { // Only one node
+        free(head);
+        head = NULL; // Set head to NULL
+        printf("Node deleted from the end.\n");
+        return;
+    }
+    Node* temp = head;
+    while (temp->next->next != NULL) { // Traverse to the second-to-last node
+        temp = temp->next;
+    }
+    free(temp->next); // Free the last node
+    temp->next = NULL; // Set the second-to-last node's next to NULL
+    printf("Node deleted from the end.\n");
+
+}
+
+void DeleteFromAnyPosition(int pos){
+    if (head == NULL) {
+        printf("List is empty, nothing to delete!\n");
+        return;
+    }
+    if (pos < 1) {
+        printf("Invalid Position!!!!\n");
+        return;
+    }
+    if (pos == 1) {
+        DeleteNodeFromBeg(); // If position is 1, delete from the beginning
+        return;
+    }
+
+    Node* temp = head;
+    for (int i = 1; i < pos - 1 && temp != NULL; i++) {
+        temp = temp->next; // Traverse to the node before the position
+    }
+
+    if (temp == NULL || temp->next == NULL) {
+        printf("Invalid Position!!!!\n");
+        return;
+    }
+
+    Node* nodeToDelete = temp->next; // Node to be deleted
+    temp->next = nodeToDelete->next; // Bypass the node to be deleted
+    free(nodeToDelete); // Free the memory of the deleted node
+    printf("Node deleted from position %d.\n", pos);
+}
+
+void search(int val){
+    if(head == NULL){
+        return false;
+    }
+    Node* temp = head;
+    while (temp != NuLL && temp->data != val){
+        temp = temp->next;
+    }
+    if(temp == NULL)return false;
+    return true;
+}
+
+void sort(){
+    for(Node* i = head; i->next != NULL; i=i->next){
+        Node* j;
+        for(j = head ; j->next != last; j = j->next){
+            if(j->data > j->next->data){
+                int temp = j->data;
+                j->data = j->next->data;
+                j->next->data = temp;
+            }
+        }
+        last = j;
+    }
+    printf("List Elements Sorted Successfully!!!!\n");
+}
+
 void display() {
     if (head == NULL) {
         printf("List is empty !!!\n");
@@ -80,8 +169,7 @@ void display() {
 }
 
 int main() {
-    while(1) 
-    {
+    while(1) {
         printf("---------Singly Linked List---------\n");
         printf("1. Insert at beginning\n");
         printf("2. Insert at end\n");
@@ -96,7 +184,6 @@ int main() {
         int choice, value, pos;
         printf("Enter your choice: ");
         scanf("%d",&choice);
-      
         switch (choice)
         {
             case 1:
@@ -104,13 +191,11 @@ int main() {
                 scanf("%d",&value);
                 InsertNodeInBeg(value);
             break;
-          
             case 2:
                 printf("Enter Value: ");
                 scanf("%d",&value);
                 InsertNodeAtEnd(value);
             break;
-          
             case 3:
                 printf("Enter Value: ");
                 scanf("%d",&value);
@@ -118,29 +203,32 @@ int main() {
                 scanf("%d", &pos);
                 InsertNodeAtAnyPosition(value, pos);
             break;
-          
             case 4:
+                DeleteNodeFromBeg(value);
             break;
-          
             case 5:
+                DeleteNodeFromEnd(value);
             break;
-          
             case 6:
+                printf("Enter position : ");
+                scanf("%d",&pos);
+                DeleteFromAnyPosition(pos);
             break;
-          
             case 7:
+                printf("Enter value to search : ");
+                scanf("%d",&val);
+                if (Search(val)) printf ("Element found!!!!\n");
+                else printf("Element not found!!!!\n");
             break;
-          
             case 8:
+                sort()
             break;
-          
             case 9:
+                display();
             break;
-          
             case 10:
             exit(0);
             break;
-          
             default:
             printf("Invalid choice\n");
             break;
